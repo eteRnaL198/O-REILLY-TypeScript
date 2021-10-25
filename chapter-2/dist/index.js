74e67028502c172bb865c88256157c41ed1e5c37
eteRnaL198 <ikuya313198@gmail.com>
2021-10-25 09:57:41 +0900

    Learned

---
+++ chapter-2/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let a = 1 + 2;
+let b = a + "b";
+let c = {
+    apple: a,
+    banana: b
+};
+let d = c.apple * 3;
+console.log(a, b, c, d);
+//# sourceMappingURL=index.js.map
---
+++ chapter-2/src/index.ts
@@ -0,0 +1,9 @@
+let a = 1 + 2
+let b = a +3
+let c = {
+  apple: a,
+  banana: b
+}
+let d = c.apple * 3
+
+console.log(a, b, c, d)
